06d0b429305700b0619c11262bccee28ddca784e
Chris Harvey <chris@chrisnharvey.com>
2014-10-19 21:23:04 +0100

    Inject travis client into constructor

--- src/Cottidae.php
+++ src/Cottidae.php
@@ -2,8 +2,15 @@
 
 namespace Cottidae;
 
+use Travis\Client as Travis;
+
 class Cottidae
 {
+    public function __construct(Travis $travis)
+    {
+        $this->travis = $travis;
+    }
+
     protected function encrypt($key, $data)
     {
         openssl_public_encrypt($data, $encrypted, $key);
